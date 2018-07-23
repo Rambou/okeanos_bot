2a83dfd8973a3c50de2e4e3e2fbeacce8da88c31
Nikolas Bousios <nikolaos.bousios@sword-group.com>
2018-07-24 02:52:38 +0300

    Added multiplue organization's support * code comments also have been added *more documentation added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Automate bot for GRNET's Okeanos
 This is an automated bot using selenium to authenticate your account/s to Okeanos GRNET and retrieve some of the user's information. The tool takes as input an array of your account/s. It generates a file with your account/s API Token keys, so you can use them later if you want.  
-_**Note**: this script will currently work for users from university of the aegean. If you are from another department it will need a little bit of modification. (I promise it will be few lines of code :D )_
+_**Note**: this script is optimized to work and tested for users from University of the Aegean. Probably it will work for all other organization's login pages, but if it doesn't then a little bit of modification may be needed. (I promise it will be few lines of code :D )_
 
 # How to use
 IN order to use this tool you need to download a selenium driver. There are two ways in which you can use do this. You can either run selenium with your browser locally or run it remotely.
@@ -18,3 +18,18 @@ and then run script with
 
 ### After that See the Magic Happens!
 ![sample](https://user-images.githubusercontent.com/4427553/37987632-5fa08a52-3207-11e8-96bb-fc1c3eb81387.gif)
+
+## Configuration
+### Credentials 
+Inside project a credentials.json is located where you should easily fill it with your credentials. Notice that it can support more than one user's as it's an array. See the example below.
+```
+[
+  {
+    "name": "Nikolaos",
+    "username": "rambou",
+    "password": "mySup3rP@ssw0rd",
+    "organization": "aegean"
+  }
+]
+```
+**Organization** can be anything as long as the output of autocomplete dropdown in AAI return one result (organization you want to log onto).
--- index.js
+++ index.js
@@ -17,21 +17,25 @@ async function login(user) {
         new Builder().forBrowser('firefox').usingServer('http://localhost:4444/wd/hub').build();
     let actions = driver.actions({bridge: true});
     try {
+        // Invoke login button from okeanos website and wait until GRNET's AAI page load 
         await driver.get('https://accounts.okeanos.grnet.gr/ui/login');
         await  driver.findElement(By.xpath('/html/body/div[2]/div/div[3]/div/div[1]/div/div[1]/a'))
             .sendKeys('webdriver', Key.ENTER);
         await  driver.wait(until.titleIs('AAI Federation for the Ministry of Education • Select your Home Organization'), 10000);
 
+        // find organization selection dropdown, fill and select user's organization
         await driver.wait(until.elementLocated(By.className('select2-selection__placeholder')), 10000);
 
         var select = await driver.findElement(By.className('select2-selection select2-selection--single'));
 
-        await actions.click(select).sendKeys('aegean', Key.ENTER).perform();
+        await actions.click(select).sendKeys(user.organization, Key.ENTER).perform();
 
         await  driver.findElement(By.xpath('/html/body/div/div[1]/div[2]/div/form/div[1]/div[2]/button'))
             .sendKeys('webdriver', Key.ENTER);
         await  driver.wait(until.titleIs('Central Authentication Service'), 10000);
 
+        // locate username and password fields in Organization's login page
+        // *code below configured for aegean, maybe it will not Work for others
         await driver.wait(until.elementLocated(
             By.className('form-input')), 10000);
 
@@ -43,12 +47,13 @@ async function login(user) {
 
         await driver.findElement(By.xpath('//*[@id="submitForm"]'))
             .sendKeys('webdriver', Key.ENTER);
-
+        // wait for okeanos dashboard to load
         await driver.wait(until.titleIs('~okeanos Dashboard | Overview'), 10000)
             .catch(() => {
                 console.log('Authentication for user ' + Users[user].name + ' failed! :(');
             });
 
+        // retrieve user's token information
         await driver.get('https://accounts.okeanos.grnet.gr/ui/api_access');
 
         await driver.wait(until.elementLocated(By.name('auth_token')), 10000);
