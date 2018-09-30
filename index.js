cf4968c5d41bc70843b547af4419ef6aaddb3019
Nikolas Bousios <nikolaos.bousios@sword-group.com>
2018-09-30 14:16:48 +0300

    async queue added with configurable limit of concurrency

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {
+    Builder,
+    By,
+    Key,
+    until
+} = require('selenium-webdriver');
 const fs = require('fs');
+const async = require('async');
 
 // read credentials from local file
 var Users = JSON.parse(fs.readFileSync(process.env.CREDENTIALS_FILE || './credentials.json', "utf8"));
 
 var tokens = [];
 
-// loop through its user asynchronously
-for (const user in Users) {
-    (login)(user);
-}
+// create a queue object with concurrency 10
+var q = async.queue(login, process.env.LIMIT_CONCURRENT || 10);
 
-async function login(user) {
-    console.log('Running for user: ' + Users[user].name);
+// assign a callback
+q.drain = function () {
+    console.log('All logins have been executed!');
+};
+
+// add some items to the queue (batch-wise)
+q.push(Users, function (err) {
+    console.log('finished processing item');
+});
+
+async function login(user, callback) {
+    console.log('Running for user: ' + user.name);
     let driver = await
-        new Builder().forBrowser(process.env.BROWSER || 'firefox').usingServer(process.env.SELENIUM_SERVER || 'http://localhost:4444/wd/hub').build();
-    let actions = driver.actions({bridge: true});
+    new Builder().forBrowser(process.env.BROWSER || 'firefox').usingServer(process.env.SELENIUM_SERVER || 'http://localhost:4444/wd/hub').build();
+    let actions = driver.actions({
+        bridge: true
+    });
     try {
         // Invoke login button from okeanos website and wait until GRNET's AAI page load 
         await driver.get('https://accounts.okeanos.grnet.gr/ui/login');
-        await  driver.findElement(By.xpath('/html/body/div[2]/div/div[3]/div/div[1]/div/div[1]/a'))
+        await driver.findElement(By.xpath('/html/body/div[2]/div/div[3]/div/div[1]/div/div[1]/a'))
             .sendKeys('webdriver', Key.ENTER);
-        await  driver.wait(until.titleIs('AAI Federation for the Ministry of Education • Select your Home Organization'), 10000);
+        await driver.wait(until.titleIs('AAI Federation for the Ministry of Education • Select your Home Organization'), 10000);
 
         // find organization selection dropdown, fill and select user's organization
         await driver.wait(until.elementLocated(By.className('select2-selection__placeholder')), 10000);
 
         var select = await driver.findElement(By.className('select2-selection select2-selection--single'));
 
-        await actions.click(select).sendKeys(Users[user].organization, Key.ENTER).perform();
+        await actions.click(select).sendKeys(user.organization, Key.ENTER).perform();
 
-        await  driver.findElement(By.xpath('//*[@id="idp_selection"]/div[1]/div[2]/button'))
+        await driver.findElement(By.xpath('//*[@id="idp_selection"]/div[1]/div[2]/button'))
             .sendKeys('webdriver', Key.ENTER);
-        await  driver.wait(until.titleIs('Central Authentication Service'), 10000);
+        await driver.wait(until.titleIs('Central Authentication Service'), 10000);
 
         // locate username and password fields in Organization's login page
         // *code below configured for aegean, maybe it will not Work for others
@@ -40,42 +56,42 @@ async function login(user) {
             By.className('form-input')), 10000);
 
         await driver.findElement(By.xpath('//*[@id="username"]'))
-            .sendKeys(Users[user].username);
+            .sendKeys(user.username);
         await driver.findElement(By.xpath('//*[@id="password"]'))
-            .sendKeys(Users[user].password);
+            .sendKeys(user.password);
 
 
         await driver.findElement(By.xpath('//*[@id="submitForm"]'))
             .sendKeys('webdriver', Key.ENTER);
 
         // wait for okeanos dashboard to load
-        try{
+        try {
             await driver.wait(until.titleIs('~okeanos Dashboard | Overview'), 10000);
-        }catch (e) {
+        } catch (e) {
             var confirm = await driver.wait(until.elementLocated(By.name('confirm')), 3000)
-            .catch(() => {
-                console.log('Authentication for user ' + Users[user].name + ' failed! :(');
-            });
-            await confirm.sendKeys('webdriver', Key.ENTER); 
+                .catch(() => {
+                    console.log('Authentication for user ' + user.name + ' failed! :(');
+                });
+            await confirm.sendKeys('webdriver', Key.ENTER);
         }
-        
+
         // retrieve user's token information
         await driver.get('https://accounts.okeanos.grnet.gr/ui/api_access');
 
         await driver.wait(until.elementLocated(By.name('auth_token')), 10000);
- 
+
         var token = await driver.findElement(By.name('auth_token')).getAttribute('value');
-        
+
         // retrieve Opestack API Username/password
         var api_username = await driver.findElement(By.xpath('(//span[@class=\'user-data\'])[1]')).getText();
         var api_password = await driver.findElement(By.xpath('(//span[@class=\'user-data\'])[2]')).getText();
 
         // pass properties to user's object
-        Users[user].token = token;
-        Users[user].api_username = api_username;
-        Users[user].api_password = api_password;
+        user.token = token;
+        user.api_username = api_username;
+        user.api_password = api_password;
 
-        await tokens.push(Users[user]);
+        await tokens.push(user);
 
         // write the updated users object with Token to the credentials.json file
         await fs.writeFile(
@@ -93,6 +109,6 @@ async function login(user) {
             'portal not Working? Maybe something else then. :\\');
     } finally {
         await driver.quit();
+        await callback();
     }
-
 }
